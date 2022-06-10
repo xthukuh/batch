38ebb7c0fcc6b24fa800b6612bb80abd02539d43
Thuku <xthukuh@gmail.com>
2022-06-10 11:12:53 +0300

    latest

--- test-nl.bat
+++ test-nl.bat
@@ -1,19 +1,23 @@
 @echo off
-setlocal EnableDelayedExpansion
-(set \n=^
-%=This is Mandatory Space=%
+REM https://stackoverflow.com/questions/1721258/is-it-possible-to-put-a-new-line-character-in-an-echo-line-in-a-batch-file
+SetLocal EnableDelayedExpansion
+
+(set NL=^
+%=DONT REMOVE THIS=%
 )
-set NL=^%\n%%\n%
-echo Hello!\n!World
-echo Hello!NL!World
+
+echo NL1 Hello!NL!World
+echo NL2 Hello!nl!World
+set "str=Hello!nl!World"
+set "str=!str! Hello!nl!World"
+echo str "!str!"
 
 if "!NL!" NEQ "!NL:~0,1!" echo Error "Linefeed definition is defect, probably multiple invisble whitespaces at the line end in the definition of NL"
 
 FOR /F "delims=" %%n in ("!NL!") do (
 	echo Error "Linefeed definition is defect, probably invisble whitespaces at the line end in the definition of NL"
 )
-echo "Line`nNewline"
-echo "Hello world.%NL%Hello world."
+echo "Hello world.!NL!Hello world."
 EndLocal & (
 	if "%~1" neq "" set "%~1=!NL!"
 ) & exit /b
